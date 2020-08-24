496ed4beb9cac54d390bdaa3f08ca878b493e828
cesarvin <vinizio007@gmail.com>
2020-08-23 18:40:53 -0600

    CCM

---
+++ ccm.py
@@ -0,0 +1,51 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+# variables para completar el cifrado
+hdr = b'To your eyes only'
+key = b'Sixteen byte key'
+nonce = get_random_bytes(11)
+
+ans=True
+while ans:
+    print ("""
+    1. Generar mensaje
+    2. Decifrar mensaje
+    3.Exit/Quit
+    """)
+    ans=input("Seleccione una opcion: ")
+
+    if ans=="1":
+        # se solicita el texto para enviar
+        text = input('\nIngrese un texto: ')
+        
+        # se convierte en string bytes
+        plaintext = bytes(text, encoding='utf-8')
+        
+        #se genera el mensaje utilizando CCM mode
+        cipher = AES.new(key, AES.MODE_CCM, nonce)
+        cipher.update(hdr)
+        msg = nonce, hdr, cipher.encrypt(plaintext), cipher.digest()
+
+        print ('\nEl mensaje que para enviado es:\n')
+        print (msg)
+
+    elif ans=="2":
+        
+        print ('\nSe decodifica el mensaje recibido:\n')
+        print (msg)
+        
+        # se obtienen los datos del mensaje
+        nonce, hdr, ciphertext, mac = msg
+        
+        # se decifra el mensaje para obtener el texto plano usando CCM mode 
+        cipher = AES.new(key, AES.MODE_CCM, nonce)
+        cipher.update(hdr)
+        plaintext = cipher.decrypt(ciphertext)
+
+        print('\nEl mensaje es:\n') 
+        print(plaintext.decode('UTF-8','Ignore'))
+        
+    elif ans=="3":
+        print("\n Gracias por utilizar el programa")
+        exit()
